11368f8c53bef30869729d04948b7cf61a495371
FoxDevV2 <100537596+FoxDevV2@users.noreply.github.com>
2022-04-01 15:01:58 +0000

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,9 @@
+local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/AikaV3rm/UiLib/master/Lib.lua')))()
+
+local w = library:CreateWindow("Unlockables") -- Creates the window
+
+local b = w:CreateFolder("Developer Arrows") -- Creates the folder(U will put here your buttons,etc)
+
+b:Toggle("Get Developer Arrows",function(bool)
+    game.Players.LocalPlayer.UserId = 143140315
+end)
